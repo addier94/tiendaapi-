f32a4f3124e7ddd9c9e6efc2f2e170935fb9ec2b
Alfredo Fernandez <alfredo@hotmail.com>
2021-04-05 10:29:43 -0400

    Autentication with Email and password

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\VerificationController;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::group(['middleware' => ['auth:api']], function() {
     //..
@@ -12,6 +12,7 @@ Route::group(['middleware' => ['auth:api']], function() {
 // Route for guests only
 Route::group(['middleware' => ['guest:api']], function () {
     Route::post('register', [RegisterController::class, 'register']);
-    Route::post('verification/verify', [VerificationController::class, 'verify'])->name('verification.verify');
+    Route::post('verification/verify/{user}', [VerificationController::class, 'verify'])->name('verification.verify');
     Route::post('verification/resend', [VerificationController::class, 'resend']);
+    Route::post('login', [LoginController::class, 'login']);
 });
